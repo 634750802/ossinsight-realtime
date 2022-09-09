46e7f8e96a15b9e54c44b3447299b17ebed475f8
634750802 <634750802@qq.com>
2022-09-09 03:01:02 +0000

    634750802 published a site update

--- assets/gh-repo.vue_vue_type_script_setup_true_lang.f3cf977d.js
+++
@@ -1,1 +0,0 @@
-import{f as n,o,c as a,t as r}from"./index.8f380576.js";const s=["href"],_=n({__name:"gh-repo",props:{name:null},setup(t){const e=t;return(c,p)=>(o(),a("a",{href:`https://github.com/${e.name}`,target:"_blank"},r(e.name),9,s))}});export{_};
